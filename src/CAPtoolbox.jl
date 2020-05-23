d7f6bb7365e5be7e040ce991e2c3b66972e0ad3c
Tehran Davis <davtj@ucmail.uc.edu>
2020-05-23 00:15:39 -0400

    Files generated by PkgTemplates

---
+++ src/CAPtoolbox.jl
@@ -0,0 +1,5 @@
+module CAPtoolbox
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CAPtoolbox
+using Test
+
+@testset "CAPtoolbox.jl" begin
+    # Write your own tests here.
+end
