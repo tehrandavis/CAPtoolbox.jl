1f8867c83b3807f6a8d34741170569dfe21806a7
Tehran Davis <tehran.davis@uc.edu>
2023-02-14 13:30:20 -0500

    updated mfdfa files

--- src/multifractal_tools/mfdfa1.jl
+++ src/multifractal_tools/mfdfa1.jl
@@ -5,10 +5,11 @@ Front. Physiol., 04 June 2012 | https://doi.org/10.3389/fphys.2012.00141
 
 ==#
 
-using StatsBase, Polynomials, DataFrames, GLM
+using StatsBase, Polynomials, DataFrames, GLM, Polynomials.PolyCompat
 
 
 function MFDFA(signal, scale, q, m)
+
 #== Multifractal detrended fluctuation analysis (MFDFA)
 [Hq,tq,hq,Dq,Fq]=MFDFA(signal,scale,q,m);
 INPUT PARAMETERS---------------------------------------------------------
@@ -16,31 +17,14 @@ signal:       input signal
 scale:        vector of scales
 q:            q-order that weights the local variations
 m:            polynomial order for the detrending
-Fig:          1/0 flag for output plot of Fq, Hq, tq and multifractal
-           spectrum (i.e. Dq versus hq).
+
 OUTPUT VARIABLES---------------------------------------------------------
 Hq:           q-order Hurst exponent
 tq:           q-order mass exponent
 hq:           q-order singularity exponent
 Dq:           q-order dimension
 Fq:           q-order scaling function
-EXAMPLE------------------------------------------------------------------
-load fractaldata
-scmin=16;
-scmax=1024;
-scres=19;
-exponents=range(log2(scmin),log2(scmax),length = scres);
-scale= round.(2 .^exponents)
-q=[-5:5;]
-m=1
-signal1=multifractal;
-signal2=monofractal;
-signal3=whitenoise;
-[Hq1,tq1,hq1,Dq1,Fq1]=MFDFA1(signal1,scale,q,m,1);
-[Hq2,tq2,hq2,Dq2,Fq2]=MFDFA1(signal2,scale,q,m,1);
-[Hq3,tq3,hq3,Dq3,Fq3]=MFDFA1(signal3,scale,q,m,1);
 --------------------------------------------------------------------==#
-
 
 
     X=cumsum(signal.-mean(signal), dims=1);
@@ -62,14 +46,15 @@ signal3=whitenoise;
     # if min(scale)<m+1
     #    error('The minimum scale must be larger than trend order m+1')
     # end
+
     for ns=1:length(scale)
         # segments[ns]=floor(length(X)/scale[ns]);
         segments = floor(length(X)/scale[ns])
         for v=1:segments
             Index=Int.([(((v-1)*scale[ns])+1):v*scale[ns];])
-            C=polyfit(Index,X[Index],m)
-            fit=polyval(C,Index)
-            push!(RMS_scale[ns],sqrt(mean((X[Index]-fit).^2)))
+            C=Polynomials.PolyCompat.polyfit(Index,X[Index],m);
+            pfit = Polynomials.PolyCompat.polyval(C,Index)
+            push!(RMS_scale[ns],sqrt(mean((X[Index]-pfit).^2)))
         end
         for nq=1:length(q)
             qRMS[nq,ns] = RMS_scale[ns].^q[nq]
@@ -96,6 +81,8 @@ signal3=whitenoise;
     hq = diff(tq)./(q[2]-q[1])
     Dq = q[1:(length(q)-1)].*hq-tq[1:(length(tq)-1)]
 
+    mfw = maximum(hq) - minimum(hq);
+
 
     output = Dict("Hq" => Hq,
                     "Hq_r2" => Hq_r2,
@@ -104,6 +91,7 @@ signal3=whitenoise;
                     "Dq" => Dq,
                     "Fq" => Fq,
                     "q" => q,
+                    "mfw" => mfw
                     )
 
     return output
